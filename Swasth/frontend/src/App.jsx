977fa1a5009eba4a6f1283be3af867fee6d494ef
Gaurav-Yadav04 <gaurav4004ayd@gmail.com.>
2026-09-14 16:23:00 +0530

    last

--- Swasth/frontend/src/App.jsx
+++ Swasth/frontend/src/App.jsx
@@ -2,15 +2,12 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-import Scan from "./pages/Scan";
+
 import AppointmentPage from "./pages/AppointmentPage";
 import PatientQueue from "./pages/PatientQueue";
 
 
 
-import PatientReview from "./pages/PatientReview";
-import Slip from "./pages/Slip";
-import SlipPreview from "./pages/SlipPreview";
 import "./App.css";
 import Profile from "./pages/Profile";
 import Footer from "./components/Footer";
@@ -25,10 +22,7 @@ export default function App() {
         <Route path="/patient" element={<PatientQueue />} />
       
         <Route path="/register" element={<Register />} />
-        <Route path="/scan" element={<Scan />} />
-        <Route path="/patient-review" element={<PatientReview />} />
-        <Route path="/slip-preview" element={<SlipPreview />} />
-        <Route path="/slip" element={<Slip />} />
+  
         <Route path="/appo" element={<AppointmentPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/profile" element={<Profile />} />
